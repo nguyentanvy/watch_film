cf676facd6303ca6cf8b3e824de12e9567dfadfe
nguyentanvy <1951120161@sv.ut.edu.vn>
2023-03-24 00:19:54 +0700

    make navigation

--- src/components/page-header/PageHeader.jsx
+++ src/components/page-header/PageHeader.jsx
@@ -5,7 +5,18 @@ import bg from '../../assets/footer-bg.jpg';
 const PageHeader = props => {
   return (
     <div className="page-header" style={{backgroundImage: `url(${bg})`}}>
-      <h2>{props.children}</h2>
+      <h2>{props.children === 'Movies' ? (
+        <>
+        <i className="bx bxs-movie cus-icon"></i>
+        Movies
+        </>
+        ) : (
+        <>
+        <i className='bx bxs-tv cus-icon'></i>TV Series
+        </>
+      )}
+      </h2>
+      {/* <h2>{props.children}</h2> */}
     </div>
   )
 }
