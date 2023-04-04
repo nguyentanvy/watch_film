ba571b088cbdc528d6f6a94d0866c503d94675e5
nguyentanvy <1951120161@sv.ut.edu.vn>
2023-04-05 01:13:23 +0700

    social

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -11,6 +11,8 @@ import Button from '../../components/button/Button';
 
 const Detail = () => {
   // window.scrollTo(0, 0);
+  const currentUrl = window.location.href;
+  const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`;
   const { category, id } = useParams();
   let navigate = useNavigate();
     const [item, setItem] = useState(null);
@@ -54,11 +56,104 @@ const Detail = () => {
                   ))
                 }
               </div>
-              {
-                      <Button className="watch-btn" onClick={() => navigate("/"+category+"/watch/"+item.id)}>
-                                      Watch now
-                      </Button>
+
+              <div className="module-btn">    
+                {
+                    <Button className="watch-btn" onClick={() => navigate("/"+category+"/watch/"+item.id)}>
+                                    Watch now
+                    </Button>
+                }
+                <a className="btn facebook-share-button" rel="noreferrer" href={facebookUrl} target="_blank">
+                  Share on Facebook
+                </a>
+              </div>
+
+              <div className="release-date">
+                <h3 style={{display: 'inline-block'}}>Release:</h3>
+                <p style={{display: 'inline-block'}}>
+                {/* {item.release_date
+                  ? item.release_date.toString()
+                  .substring(
+                    0,
+                    item.release_date.indexOf("-")
+                  )
+                  :
+                  item.first_air_date.toString()
+                  .substring(
+                    0,
+                    item.first_air_date.indexOf("-")
+                  )
+                  } */}
+
+                  {item.release_date
+                    ? item.release_date.toString()
+                      .substring(0, item.release_date.indexOf("-"))
+                    : item.first_air_date
+                      ? item.first_air_date.toString()
+                        .substring(0, item.first_air_date.indexOf("-"))
+                      : ""
                   }
+                  {/* Nếu nó tồn tại, toString() sẽ chuyển đổi giá trị thành chuỗi và substring() sẽ trả về một phần của chuỗi (từ vị trí bắt đầu đến vị trí kết thúc được chỉ định). Trong trường hợp này, substring() được sử dụng để lấy chuỗi các chữ số đầu tiên của ngày phát hành của phim. */}
+                </p>
+                
+              </div>
+
+              <div className="runtime">
+                <h3 style={{display: 'inline-block'}}>Runtime:</h3>
+                <p style={{display: 'inline-block'}}>
+
+                  {item.runtime
+                    ? item.runtime + 'min'
+                    : item.episode_run_time.length>0
+                      ? item.episode_run_time[0] + 'min'
+                      : "Updating"
+                  }
+                  {/* Nếu nó tồn tại, toString() sẽ chuyển đổi giá trị thành chuỗi và substring() sẽ trả về một phần của chuỗi (từ vị trí bắt đầu đến vị trí kết thúc được chỉ định). Trong trường hợp này, substring() được sử dụng để lấy chuỗi các chữ số đầu tiên của ngày phát hành của phim. */}
+                </p>
+                
+              </div>
+
+              <div className="rating">
+                <h3 style={{display: 'inline-block'}}>Rating:</h3>
+                <p title= {item.vote_count && item.vote_average ?
+              item.vote_average.toFixed(1) + " base on " +item.vote_count +" user rating"  :
+              ""
+              } 
+                
+                
+                style={{display: 'inline-block'}}>
+                  
+                  {item.vote_average
+                    ? item.vote_average.toFixed(1) + '/10'
+                    : "No voted"
+                  }
+                    {/* <p className="star" style={{display: 'inline-block'}}>
+                      {item.vote_average > 0 &&
+                      Array.from({ length: Math.floor(item.vote_average) }, (_, index) => (
+                      <i key={index} className='bx bxs-star'></i>
+                        ))
+                      }
+                    </p> */}
+                   
+                   {item.vote_average > 0 && (
+                    <p className='star' style={{ display: 'inline-block' }}>
+                      {Array.from({ length: 10 }, (_, index) => {
+                        const isYellow = index < item.vote_average.toFixed();
+                        return (
+                          <i
+                            key={index}
+                            className='bx bxs-star'
+                            style={{ color: isYellow ? '#f5c518' : 'inherit' }}
+                          ></i>
+                        );
+                      })}
+                    </p>
+                  )}
+
+                </p>
+                
+              </div>
+
               <p className="overview">{item.overview}</p>
               <div className="cast">
                 <div className="section__header">
@@ -72,6 +167,11 @@ const Detail = () => {
           <div className="container">
             <div className="section mb-3">
               <VideoList id={item.id}/>
+            </div>
+            <div className="section mb-3">
+            <div class="fb-comments" data-href={currentUrl} data-width="" data-numposts="10">
+
+            </div>
             </div>
             <div className="section mb-3">
               <div className="section__header mb-2">
